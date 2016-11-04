0a8b28eaa16258e3790ecb9faf55dc33d520a470
Frederik Rodriguez <frederikrodriguez@velocitypartners.net>
2016-11-04 17:11:55 -0500

    Added Primefaces Version

---
+++ Eleven/src/java/com/eleven/general/Config.java
@@ -0,0 +1,8 @@
+package com.eleven.general;
+
+public class Config {
+	public static final String db_driver = "com.mysql.jdbc.Driver";
+	public static final String db_url = "jdbc:mysql://localhost/eleven";
+	public static final String db_user = "root";
+	public static final String db_pass = "";
+}
