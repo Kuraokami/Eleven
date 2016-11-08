6d2dd0d4d1e7ceed5dc93038ffe2037197518367
Frederik Rodriguez <frederikrodriguez@velocitypartners.net>
2016-11-08 15:13:58 -0500

    Removing JHipster Version

--- JHipster/src/main/java/com/nethard/eleven/web/rest/vm/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * View Models used by Spring MVC REST controllers.
- */
-package com.nethard.eleven.web.rest.vm;
--- JHipster/src/main/webapp/app/components/form/pagination.constants.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    'use strict';
-
-    angular
-        .module('futBalApp')
-        .constant('paginationConstants', {
-            'itemsPerPage': 20
-        });
-})();
